d1f2f8a330cbccc77a3fc6fe89e561f1f67e9b46
Hyunsoo Shin <2091126@hansung.ac.kr>
2024-05-21 19:26:06 +0900

    Feat. Google Authentication.

--- Home.py
+++ Home.py
@@ -17,3 +17,29 @@ st.markdown(
     > * 연락하기 [메일](mailto:2091126@hansung.ac.kr)
     """
 )
+import streamlit as st
+from streamlit_google_auth import Authenticate
+
+google_credentials = st.secrets["GOOGLE_CREDENTIALS"] 
+
+st.title('Streamlit Google Auth Example')
+
+authenticator = Authenticate(
+    secret_credentials_path = 'google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri = 'http://localhost:8501',
+)
+
+# Catch the login event
+authenticator.check_authentification()
+
+# Create the login button
+authenticator.login()
+
+if st.session_state['connected']:
+    st.image(st.session_state['user_info'].get('picture'))
+    st.write('Hello, '+ st.session_state['user_info'].get('name'))
+    st.write('Your email is '+ st.session_state['user_info'].get('email'))
+    if st.button('Log out'):
+        authenticator.logout()
