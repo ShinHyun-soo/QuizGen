f972c78b91498fd5b92978d1f3b2855b621a3515
Hyunsoo Shin <2091126@hansung.ac.kr>
2024-05-24 22:58:05 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,28 +19,21 @@ st.markdown(
 )
 import streamlit as st
 from streamlit_google_auth import Authenticate
-import json
+import streamlit as st
+from streamlit_oauth import OAuth2Component
 
-google_credentials = st.secrets["GOOGLE_CREDENTIALS"]
+AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/auth"
+TOKEN_URL = "https://oauth2.googleapis.com/token"
+#REVOKE_URL = <YOUR REVOKE URL>
+CLIENT_ID = "252051893046-jkhn58c4217hougj5t0f06irbnepue0a.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
+REDIRECT_URI = "https://hsu-quizgen.streamlit.app"
+SCOPE = "openid email profile"
 
-st.title('Streamlit Google Auth Example')
+oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZATION_URL, TOKEN_URL, TOKEN_URL) #REVOKE_URL)
 
-authenticator = Authenticate(
-    secret_credentials_path = google_credentials,
-    cookie_name='my_cookie_name',
-    cookie_key='this_is_secret',
-    redirect_uri = 'http://hsu-quizgen.streamlit.app',
-)
-
-# Catch the login event
-authenticator.check_authentification()
-
-# Create the login button
-authenticator.login()
-
-if st.session_state['connected']:
-    st.image(st.session_state['user_info'].get('picture'))
-    st.write('Hello, '+ st.session_state['user_info'].get('name'))
-    st.write('Your email is '+ st.session_state['user_info'].get('email'))
-    if st.button('Log out'):
-        authenticator.logout()
+if 'token' not in st.session_state:
+  result = oauth2.authorize_button("Continue with Google", REDIRECT_URI, SCOPE, icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E")
+  if result:
+    st.session_state.token = result.get('token')
+    st.rerun()
